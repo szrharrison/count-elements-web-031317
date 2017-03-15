bb3fc396a7d3a18d8df556d2a408c9d3c8c0492f
szrharrison <s.z.r.harrison@gmail.com>
2017-03-14 23:56:57 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,9 @@
+require "pry"
 def count_elements(array)
   # code goes here
+  animals = array.uniq
+  animal_count = animals.map do |animal|
+    { animal => array.count( animal ) }
+  end
+  animal_count.reduce( :merge )
 end
- 
